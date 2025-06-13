a085fd92c6ec7ae4d0325734333141e1b2117ac4
Jordan Uggla <jordan.uggla@gmail.com>
2025-06-13 08:58:26 -0700

    Fix missing '/etc' in path

--- prepare_for_conversion.sh
+++ prepare_for_conversion.sh
@@ -3,10 +3,10 @@
 set -ex
 
 # Ensure that btrfs-convert, the binary, is included in the initramfs
-sudo cp initramfs_hook.sh /initramfs-tools/hooks/btrfs-convert-binary.sh
+sudo cp initramfs_hook.sh /etc/initramfs-tools/hooks/btrfs-convert-binary.sh
 
 # Add our conversion script to the scripts run in the initramfs before the root FS is mounted
-sudo cp premount_convert-to-btrfs.sh /initramfs-tools/scripts/init-premount/convert-to-btrfs.sh
+sudo cp premount_convert-to-btrfs.sh /etc/initramfs-tools/scripts/init-premount/convert-to-btrfs.sh
 
 # Make sure the btrfs kernel module gets included in the initramfs
 echo "btrfs" | sudo tee -a /etc/initramfs-tools/modules
