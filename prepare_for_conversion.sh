923e94a8a8366a5ae0749aa86919f4cd4168315b
Jordan Uggla <jordan.uggla@gmail.com>
2025-06-13 17:41:00 -0700

    Make prepare_for_conversion.sh handle grub issues too

--- prepare_for_conversion.sh
+++ prepare_for_conversion.sh
@@ -17,3 +17,17 @@ sudo sed -i 's/ext4/btrfs/' /etc/fstab
 
 # Actually generate new initramfs with the above changes!
 sudo update-initramfs -c -k all
+
+# Get Ubuntu's secure boot tiny grub.cfg to seach by label (which will stay the
+# same between the ext4 FS and the btrfs FS) instead of by filesystem UUID
+# (which will change)
+sudo sed -i 's/search.fs_uuid.*/search.fs_label cloudimg-rootfs root' /boot/EFI/ubuntu/grub.cfg
+
+# Make grub's own search commands search by label, until the next time
+# update-grub gets run, by which point it should correctly grab the UUID of the
+# new btrfs filesystem as we want it to anyway.
+sudo sed -i 's/--fs-uuid --set=root .*/--fs-label --set=root cloudimg-rootfs/' /boot/grub/grub.cfg
+
+# Make the kernel parameter grub passess to the kernel use label rather than
+# UUID
+sudo sed -i 's/root=UUID=.* ro /root=LABEL=cloudimg-rootfs ro /' /boot/grub/grub.cfg
