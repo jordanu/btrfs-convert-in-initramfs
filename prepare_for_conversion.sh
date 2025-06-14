c7f4b5185af1c62c410e007bb8afecc152c7b544
Jordan Uggla <jordan.uggla@gmail.com>
2025-06-13 17:40:25 -0700

    Line wrap comment in prepare_for_conversion.sh

--- prepare_for_conversion.sh
+++ prepare_for_conversion.sh
@@ -5,7 +5,8 @@ set -ex
 # Ensure that btrfs-convert, the binary, is included in the initramfs
 sudo cp initramfs_hook.sh /etc/initramfs-tools/hooks/btrfs-convert-binary.sh
 
-# Add our conversion script to the scripts run in the initramfs before the root FS is mounted
+# Add our conversion script to the scripts run in the initramfs before the
+# root FS is mounted
 sudo cp premount_convert-to-btrfs.sh /etc/initramfs-tools/scripts/init-premount/convert-to-btrfs.sh
 
 # Make sure the btrfs kernel module gets included in the initramfs
